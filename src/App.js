d1dc7acbb67995e4e697b68957c7c35930937923
Yogesh Jayachandran <108916534+Yogeshpj@users.noreply.github.com>
2023-04-05 14:09:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Productlist from './List';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Productlist/>
+    </div>
+  );
+}
+
+export default App;
